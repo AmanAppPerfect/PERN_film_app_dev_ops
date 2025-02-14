6ec95472d9a7391db250a408e7c3dde530c74c92
Aman Bhargava <aman@appperfect.com>
2025-02-14 18:22:25 +0530

    updated the host and volume mount

--- server/data/dbConfig.js
+++ server/data/dbConfig.js
@@ -2,7 +2,7 @@ import pg from "pg";
 
 const pool = new pg.Pool({
 	user: "postgres",
-	host: "db",
+	host: "postgres-service.postgres-ns.svc.cluster.local",
 	database: "filmdb",
 	password: "postgres",
 	port: 5432,
